4dcccb1fecf553e2160423909143494c056dd2ff
ryo1374 <rxyxomu419@gmail.com>
2021-11-27 16:05:12 +0900

    実装完了

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,4 +1,5 @@
 class TextsController < ApplicationController
+  before_action :set_text, only: [:show, :edit, :update, :destroy]
 
   def index
     @nickname = current_user.nickname
@@ -18,6 +19,19 @@ class TextsController < ApplicationController
     end
   end
 
+  def destroy
+    @text.destroy
+    redirect_to root_path
+  end
+
+  def edit
+
+  end
+
+  def update
+    text.update(text_params)
+  end
+
   def show
    
   end
@@ -30,6 +44,10 @@ class TextsController < ApplicationController
   private
   def text_params
     params.require(:text).permit(:sentence, :translate, :remarks, :tag, :type_id).merge(user_id: current_user.id)
+  end
+
+  def set_text
+    @text = Text.find(params[:id])
   end
 
  
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
   def show
-   
+   @text = Text.find(params[:id])
   end
 end
